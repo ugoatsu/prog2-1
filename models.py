ba995c9ac308227146f9d37dca314876e0879dcf
Yuuki Kurosawa <j3326wjw@sit.ac.jp>
2024-12-10 16:17:04 +0900

    不要な空行を削除

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
 
-
 class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
@@ -19,7 +18,6 @@ class MyModel(nn.Module):
         x = self.flatten(x)
         logits = self.network(x)
         return logits
-    
 
     def test_accuracy(model, dataloader):
         n_corrects = 0 #正解の個数
@@ -31,8 +29,7 @@ class MyModel(nn.Module):
                 logits_batch = model(image_batch)
 
             predict_batch = logits_batch.argmax(dim=1)
-            n_corrects += (label_batch == predict_batch).sum().item()
-        
+            n_corrects += (label_batch == predict_batch).sum().item()     
         #精度を計算する
         accuracy = n_corrects / len(dataloader.dataset)
         return accuracy      
@@ -43,7 +40,6 @@ class MyModel(nn.Module):
             logits_batch = model(image_batch)
 
             loss = loss_fn(logits_batch, label_batch)
-
             #最適化
             optimizer.zero_grad()
             loss.backward() #誤差逆伝播法
@@ -61,6 +57,5 @@ class MyModel(nn.Module):
 
             loss = loss_fn(logits_batch, label_batch)
             loss_total += loss.item()
-
         #バッチ数で割って平均値を返す
         return loss_total / len(dataloader)
